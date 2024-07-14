1f92b4e821115f18a586abdf3ee351386e0bde14
Tahrim19 <tarimbilal4@gmail.com>
2024-07-14 13:36:43 -0700

    creating and using custom hooks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,22 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { useState } from 'react'
 import SearchBar from './SearchBar'
 import SelectMenu from './SelectMenu'
 import CountriesList from './CountiresList'
-import {ThemeContext} from '../contexts/ThemeContext'
+import useTheme from '../hooks/useTheme'
 
 export default function Home() {
   const [query, setQuery] = useState('')
   // const [isDark] = useOutletContext()
-  const [isDark] = useContext(ThemeContext)
+  // const [isDark] = useContext(ThemeContext)
+  const [isDark] = useTheme();
   return (
     <main className={`${isDark? 'dark': ''}`}>
       <div className="search-filter-container">
         <SearchBar setQuery={setQuery} />
         <SelectMenu />
       </div>
-      {query === 'unmount' ? '' : <CountriesList query={query} />}
+      <CountriesList query={query} />
     </main>
   )
 }
